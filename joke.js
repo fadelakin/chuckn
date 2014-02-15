70e238b2a78467a23e88271ddda9fb824b991906
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-02-14 19:18:30 -0500

    parsing ICNdb

--- joke.js
+++ joke.js
@@ -2,24 +2,24 @@ var http = require('http');
 var Future = require('futures').future;
 
 var get_joke = function() {
-	var future = Future.create();
-	var url = "http://api.icndb.com/jokes/random";
+    var future = Future.create();
+    var url = "http://api.icndb.com/jokes/random";
 
-	http.get(url, function(res) {
-		var response = '';
+    http.get(url, function (res) {
+        var response = '';
 
-		res.on('data', function(chunk) {
-			response += chunk;
-		});
+        res.on('data', function (chunk) {
+            response += chunk;
+        });
 
-		res.on('end', function() {
-			future.fulfill(undefined, JSON.parse(response).value.joke);
-		});
-	}).on('error', function(e) {
-		console.log("Got error: " + e.message);
-	});
+        res.on('end', function () {
+            future.fulfill(undefined, JSON.parse(response).value.joke);
+        });
+    }).on('error', function(e) {
+        console.log("Got error: " + e.message);
+    });
 
-	return future;
+    return future;
 }
 
 module.exports.get_joke = get_joke;
