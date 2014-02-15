e82e69968af7b564237b25246c2632ab7b4c98d9
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-02-14 19:12:04 -0500

    messing about

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var http = require('http');
+var wit = require('./wit');
+var joke = require('./joke');
+var url = require('url');
 
 http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\n');
+	var queryObject = url.parse(req.url, true).query;
+	var wit_request = wit.request_wit(queryObject.Body);
+	res writeHead(200, {'Content-Type': 'text/plain'});
+
+	wit_request.when(function(err, response) {
+		if (err) console.log(err); // handle error here
+		switch (wit.outcome.intent) {
+			case "hello":
+				res.end("Hello, how are you?");
+				break;
+			case "tell_joke":
+				joke.get_joke().when(function(err, the_joke) {
+					res.end(the_joke);
+				});
+				break;
+		}
+	});
 }).listen(8766);
 
 console.log('Server running at http://127.0.0.1:8766/');
